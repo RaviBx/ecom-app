7aaff4d5b27538e27fba17f9debe1dd54ff0172f
Ravishan Balasooriya <ravimuay@gmail.com>
2022-02-26 14:57:12 +0530

    Login function done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,10 +1,29 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    
+    //Login Function
+
+    function login(Request $req){
+
+        $user =  User::where(['email'=>$req->email])->first();
+
+        //Validating the password
+        if(!$user || !Hash::check($req->password, $user->password)){
+
+            return "Username or Password does not match";
+
+        }
+        else{
+
+            $req->session()->put('user', $user);
+            return redirect('/');
+        }
+    }
 }
+
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,18 +4,19 @@
 <div class="container custom-login">
     <div class="row">
         <div class="col-sm-4 offset-4 mt-10 pt-10">
-            <form>
+            <form action="login" method="POST">
                 <div class="mb-3">
+                    @csrf
                   <label for="exampleInputEmail1" class="form-label"><b>Email address</b></label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email">
+                  <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email">
                  
                 </div>
                 <div class="mb-3">
                   <label for="exampleInputPassword1" class="form-label"><b>Password</b></label>
-                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                  <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                 </div>
                
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">LOGIN</button>
               </form>
         </div>
     </div>
